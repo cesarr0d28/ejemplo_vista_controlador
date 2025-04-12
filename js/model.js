633a6a871fb5c9f859b3cb5ea1b199c794020589
cesarr0d28 <102396277+cesarr0d28@users.noreply.github.com>
2025-04-12 15:44:15 -0500

    Update model.js

--- js/model.js
+++ js/model.js
@@ -3,6 +3,6 @@ const dataModel = {
     data: [],
     fetchData() {
         // Simulación de carga de datos desde una API o base de datos
-        this.data = ['Dato 1', 'Dato 2', 'Dato 3', 'Dato 4', 'Dato 5', 'Dato 7', 'Dato 7'];
+        this.data = ['Dato 1', 'Dato 2', 'Dato 3', 'Dato 4', 'Dato 5', 'Dato 6', 'Dato 7'];
     },
 };
